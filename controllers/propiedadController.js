3f79b42117c5fe416581fa34f96e6097fb6f63b9
José Antonio González Morente <jgonzalezmorente@gmail.com>
2022-10-09 09:29:37 +0200

    Edición de propiedades

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -51,7 +51,7 @@ const guardar = async ( req, res ) => {
             Precio.findAll(),
         ]);
     
-        res.render( 'propiedades/crear', {
+        return res.render( 'propiedades/crear', {
             pagina: 'Crear propiedad',            
             csrfToken: req.csrfToken(),
             categorias,
@@ -62,7 +62,6 @@ const guardar = async ( req, res ) => {
     }
 
     // Crear la propiedad
-
     const { titulo, descripcion, habitaciones, estacionamiento, wc, calle, lat, lng, precio: precioId, categoria: categoriaId } = req.body;
 
     const { id: usuarioId } = req.usuario;
@@ -156,11 +155,108 @@ const almacenarImagen = async ( req, res, next ) => {
     }
 }
 
+const editar = async ( req, res ) => {
+
+    const { id } = req.params;
+
+    // Validar que la propiedad exista
+    const propiedad = await Propiedad.findByPk( id );
+
+    if ( !propiedad ) {
+        return res.redirect( '/mis-propiedades' );
+    }
+
+    // Revisar que quien visita la URL, es quien creó la propidad
+    if ( propiedad.usuarioId.toString() !== req.usuario.id.toString() ) {
+        return res.redirect( '/mis-propiedades' );
+    }
+
+    // Consultar modelo de precio y categorias    
+    const [ categorias, precios ] = await Promise.all([
+        Categoria.findAll(),
+        Precio.findAll(),
+    ]);
+
+    res.render( 'propiedades/editar', {
+        pagina: `Editar propiedad: ${ propiedad.titulo }`,
+        csrfToken: req.csrfToken(),
+        categorias,
+        precios,
+        datos: propiedad
+    });
+
+}
+
+const guardarCambios = async ( req, res ) => {
+
+    // Verificar la validación
+    let resultado = validationResult( req );
+    if ( !resultado.isEmpty() ) {
+
+        const [ categorias, precios ] = await Promise.all([
+            Categoria.findAll(),
+            Precio.findAll(),
+        ]);
+
+        return res.render( 'propiedades/editar', {
+            pagina: 'Editar propiedad',
+            csrfToken: req.csrfToken(),
+            categorias,
+            precios,
+            errores: resultado.array(),
+            datos: req.body
+        });
+    
+    }
+
+    const { id } = req.params;
+
+    // Validar que la propiedad exista
+    const propiedad = await Propiedad.findByPk( id );
+
+    if ( !propiedad ) {
+        return res.redirect( '/mis-propiedades' );
+    }
+
+    // Revisar que quien visita la URL, es quien creó la propidad
+    if ( propiedad.usuarioId.toString() !== req.usuario.id.toString() ) {
+        return res.redirect( '/mis-propiedades' );
+    }    
+
+    // Reescribir el objeto y actualizarlo
+    try {
+
+        const { titulo, descripcion, habitaciones, estacionamiento, wc, calle, lat, lng, precio: precioId, categoria: categoriaId } = req.body;
+        propiedad.set({
+            titulo,
+            descripcion,
+            habitaciones,
+            estacionamiento,
+            wc,
+            calle,
+            lat,
+            lng,
+            precioId,
+            categoriaId,
+        });
+
+        await propiedad.save();
+        res.redirect( '/mis-propiedades' );
+        
+    } catch (error) {
+        console.log(error);        
+    }
+    
+    console.log('Guardando cambios...');
+
+}
 
 export {
     admin,
     crear,
     guardar,
     agregarImagen,
-    almacenarImagen
+    almacenarImagen,
+    editar,
+    guardarCambios,
 }
--- routes/propiedadesRoutes.js
+++ routes/propiedadesRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { body } from 'express-validator';
-import { admin, crear, guardar, agregarImagen, almacenarImagen } from '../controllers/propiedadController.js';
+import { admin, crear, guardar, agregarImagen, almacenarImagen, editar, guardarCambios } from '../controllers/propiedadController.js';
 import protegerRuta from '../middlewares/protegerRuta.js';
 import upload from '../middlewares/subirImagen.js';
 
@@ -43,6 +43,35 @@ router.post( '/propiedades/agregar-imagen/:id',
     protegerRuta,
     upload.single( 'imagen' ),
     almacenarImagen
+);
+
+router.get( '/propiedades/editar/:id',
+    protegerRuta,
+    editar
+);
+
+router.post( '/propiedades/editar/:id',
+    protegerRuta,
+    
+    body( 'titulo' ).notEmpty().withMessage( 'El título del anuncio es obligatorio' ),
+    
+    body( 'descripcion' )
+        .notEmpty().withMessage( 'La descripción no puede ir vacía' )
+        .isLength({ max: 200 }).withMessage( 'La descripción es muy larga' ),
+        
+    body( 'categoria' ).isNumeric().withMessage( 'Selecciona una categoría' ),
+    
+    body( 'precio' ).isNumeric().withMessage( 'Selecciona un rango de precios' ),
+
+    body( 'habitaciones' ).isNumeric().withMessage( 'Selecciona la cantidad de habitaciones' ),
+
+    body( 'estacionamiento' ).isNumeric().withMessage( 'Selecciona la cantidad de estacionamientos' ),
+
+    body( 'wc' ).isNumeric().withMessage( 'Selecciona la cantidad de baños' ),
+
+    body( 'lat' ).notEmpty().withMessage( 'Ubica la propiead en el mapa' ),
+
+    guardarCambios 
 );
 
 export default router;
